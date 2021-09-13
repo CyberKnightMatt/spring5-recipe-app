fcbbb39f5684d805a4ff34dc926182a09b69d5f5
Matt <matt@stallings.us>
2021-09-13 17:58:26 -0500

    Initial commit.

---
+++ src/test/java/us/stallings/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package us.stallings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
